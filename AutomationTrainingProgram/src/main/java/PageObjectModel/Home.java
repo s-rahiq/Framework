bd8c6a333b20019ea7bab47d16ffacee56a4968e
User <User@Sorya>
2022-05-22 17:16:38 -0400

    Framework to automation.

--- AutomationTrainingProgram/src/main/java/PageObjectModel/Home.java
+++ AutomationTrainingProgram/src/main/java/PageObjectModel/Home.java
@@ -1,33 +1,12 @@
 package PageObjectModel;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
- 
+
 public class Home {
-	WebDriver driver;
-	//1) locating the element of a page
-	// you can find the email and pass from the fb website
-	@FindBy(name="email") WebElement Ids;
-	@FindBy(name="pass") WebElement Psswords;
-	@FindBy(linkText="Forget Password") WebElement Forgot;
-	
-  //2) Creating the constructor
-	public Home(WebDriver driver) {
- 		this.driver=driver;
-		PageFactory.initElements(driver, this);
-		
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver;
 	}
-	//3)creating a method
-	public void Email() {
-		Ids.sendKeys("John");
-		}
-	public void PsswordFieldsEnters() {
-		Psswords.sendKeys("Silver");
-	}
-	public void ForgotLinkClick() {
-		Forgot.click();
-	}
-} 
-//now let's create the second page which is the forgot password page'
+
+}
